121e55b722b6dc5acd22be1bc8d2d6e8a0836e70
GleciaGaba <gleciasf@hotmail.com>
2024-10-02 16:26:39 +0200

    update review

--- movies_back/src/main/java/dev/glecia/movies/MovieController.java
+++ movies_back/src/main/java/dev/glecia/movies/MovieController.java
@@ -1,5 +1,6 @@
 package dev.glecia.movies;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.Objects;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/movies")
@@ -23,9 +24,9 @@ public class MovieController {
         return new ResponseEntity<List<Movie>>(movieService.allMovies(), HttpStatus.OK);
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Movie> getSingleMovie(@PathVariable Objects id) {
-        return null;
+    @GetMapping("/{imdbId}")
+    public ResponseEntity<Optional<Movie>> getSingleMovie(@PathVariable String imdbId) {
+        return new ResponseEntity<Optional<Movie>>(movieService.singleMovie(imdbId), HttpStatus.OK);
     }
 
 }
--- movies_back/src/main/java/dev/glecia/movies/Review.java
+++ movies_back/src/main/java/dev/glecia/movies/Review.java
@@ -5,7 +5,6 @@ import org.springframework.data.annotation.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "reviews")
@@ -17,4 +16,8 @@ public class Review {
     private ObjectId id;
 
     private String body;
+
+    public Review(String body) {
+        this.body = body;
+    }
 }
